71555a16b4acd033b0dfa0a758817c7a64264091
ryandonaghue <ryandonaghue@outlook.com>
2022-11-03 16:04:39 +0000

    Created BMI Kata quesiton

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-/* 
-👉 Write your kata here!
-*/
+// 👉 Your task is to complete the BMI calculator function. 
+// The function should take two arguments 🫃 Weight(KG), 📐Height(M).
+// The function should return result of numbers after applying the calculation.
 
-//👉 Write the function your CodeWarriors will start with below here:
+// Examples( Weight, Height) --> output
+// (80, 1.8) = 24.69
+// (60, 1.6) = 23.4
+// (120, 2.2) = 27.2
+// round answer to the nearest integer 💯
+
+export function bmiCalculator(weight, height) {
+    //your code
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,21 @@
 //👉 Write your tests below here:
+import{expect, test}from '@jest/globals'
+import { bmiCalculator } from './main.js'
+
+test ('check example 1',() => {
+    const actual = bmiCalculator(80, 1.8)
+    const expected = 25
+    expect(actual).toStrictEqual(expected);
+})
+
+test ('check example 1',() => {
+    const actual = bmiCalculator(60, 1.6)
+    const expected = 23
+    expect(actual).toStrictEqual(expected);
+})
+
+test ('check example 1',() => {
+    const actual = bmiCalculator(120, 2.2)
+    const expected = 25
+    expect(actual).toStrictEqual(expected);
+})
